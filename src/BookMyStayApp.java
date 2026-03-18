1cdc82ca786fdc6a8e9dff26af2506dce999963a
rupa-9777 <rupasriputta9777@gmail.com>
2026-03-18 22:44:13 +0530

    UC2: Basic room types with inheritance and static availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,11 +1,72 @@
-public class BookMyStayApp{
+abstract class Room {
+
+    String type;
+    int beds;
+    int size;
+    double price;
+
+    Room(String type, int beds, int size, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    void displayRoom() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq.ft");
+        System.out.println("Price per night: " + price);
+    }
+}
+
+class SingleRoom extends Room {
+
+    SingleRoom() {
+        super("Single Room", 1, 200, 2000);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    DoubleRoom() {
+        super("Double Room", 2, 350, 3500);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    SuiteRoom() {
+        super("Suite Room", 3, 600, 7000);
+    }
+}
+
+public class BookMyStayApp {
 
     public static void main(String[] args) {
 
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
+
         System.out.println("=================================");
         System.out.println("        Book My Stay App");
-        System.out.println("    Hotel Booking System v1.0");
+        System.out.println("    Hotel Booking System v2.1");
         System.out.println("=================================");
-        System.out.println("Welcome to the Hotel Booking Management System");
+
+        single.displayRoom();
+        System.out.println("Available: " + singleAvailable);
+        System.out.println();
+
+        doubleRoom.displayRoom();
+        System.out.println("Available: " + doubleAvailable);
+        System.out.println();
+
+        suite.displayRoom();
+        System.out.println("Available: " + suiteAvailable);
     }
 }
