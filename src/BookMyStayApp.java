84e4cc1e5b0c496b9678ab9e28311943fe911864
rupa-9777 <rupasriputta9777@gmail.com>
2026-03-18 22:48:19 +0530

    UC5: FIFO booking request queue using Queue

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,60 +1,40 @@
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
-abstract class Room {
+class Reservation {
 
-    String type;
-    int beds;
-    int size;
-    double price;
+    String guestName;
+    String roomType;
 
-    Room(String type, int beds, int size, double price) {
-        this.type = type;
-        this.beds = beds;
-        this.size = size;
-        this.price = price;
-    }
-
-    void displayRoom(int available) {
-        System.out.println(type + ":");
-        System.out.println("Beds: " + beds);
-        System.out.println("Size: " + size + " sqft");
-        System.out.println("Price per night: " + price);
-        System.out.println("Available: " + available);
-        System.out.println();
+    Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
     }
 }
 
-class SingleRoom extends Room {
-    SingleRoom() {
-        super("Single Room", 1, 250, 1500.0);
-    }
-}
+class BookingRequestQueue {
 
-class DoubleRoom extends Room {
-    DoubleRoom() {
-        super("Double Room", 2, 400, 2500.0);
-    }
-}
+    private Queue<Reservation> queue = new LinkedList<>();
 
-class SuiteRoom extends Room {
-    SuiteRoom() {
-        super("Suite Room", 3, 750, 5000.0);
-    }
-}
-
-class RoomInventory {
-
-    private HashMap<String, Integer> inventory;
-
-    RoomInventory() {
-        inventory = new HashMap<>();
-        inventory.put("Single Room", 5);
-        inventory.put("Double Room", 3);
-        inventory.put("Suite Room", 2);
+    void addRequest(Reservation reservation) {
+        queue.add(reservation);
     }
 
-    int getAvailability(String roomType) {
-        return inventory.getOrDefault(roomType, 0);
+    void processRequests() {
+
+        System.out.println("Booking Request Queue");
+
+        while (!queue.isEmpty()) {
+
+            Reservation r = queue.poll();
+
+            System.out.println(
+                    "Processing booking for Guest: " +
+                            r.guestName +
+                            ", Room Type: " +
+                            r.roomType
+            );
+        }
     }
 }
 
@@ -62,25 +42,12 @@ public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Room Search\n");
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
 
-        RoomInventory inventory = new RoomInventory();
+        bookingQueue.addRequest(new Reservation("Abhi", "Single"));
+        bookingQueue.addRequest(new Reservation("Subha", "Double"));
+        bookingQueue.addRequest(new Reservation("Vanmathi", "Suite"));
 
-        Room single = new SingleRoom();
-        Room doubleRoom = new DoubleRoom();
-        Room suite = new SuiteRoom();
-
-        int singleAvailable = inventory.getAvailability("Single Room");
-        int doubleAvailable = inventory.getAvailability("Double Room");
-        int suiteAvailable = inventory.getAvailability("Suite Room");
-
-        if(singleAvailable > 0)
-            single.displayRoom(singleAvailable);
-
-        if(doubleAvailable > 0)
-            doubleRoom.displayRoom(doubleAvailable);
-
-        if(suiteAvailable > 0)
-            suite.displayRoom(suiteAvailable);
+        bookingQueue.processRequests();
     }
 }
